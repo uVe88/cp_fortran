9deba18acef139887794810d45069c8fccdf7d81
vgomezg <vgomezg@aqualogy.net>
2016-07-13 11:07:27 +0200

    práctica 1 anillo rc1

--- ANILLO/master.f
+++ ANILLO/master.f
@@ -1,35 +1,46 @@
 c Programa master - practica 1 programación paralela
 c Alumno Ivan Gomez
-       	program master
-       	implicit none
-      	include 'fpvm3.h'
-c Declaración de variables
-       	integer nhost, numt, tids, info, var
+      program master
+      implicit none
+      include 'fpvm3.h'
 
-c Lee la variable de entrada
+c Declaración de variables
+      integer nhost, numt, tids, info, var, mytid
 
 c Inicializa PVM
+	  call pvmfmytid(mytid)
+
+c Lee la variable de entrada
+      print *,'Introduce un número entero:'
+      read *, var
 
 c Expande los procesos hijos
-        call expande('slave', tids, nhost, numt)
+      call expande('slave', tids, nhost, numt)
 
-c Inicializa el buffer para el intercambio y envía la información
-c Envia:
-c   	- tid del proceso padre
-c	- tids de los procesos disponibles (esclavos)
-c  	- valor del input a incrementar
-c	- 
-	call pvmfinitsend(PVMDATADRAW, 1)
-	call pvmfpack(INTEGER2, var, 1, 1, info)
+c Comprobamos que tenemos más se ha distribuido a más de un host
+	  if (nhost.lt.0) then
 
-	
+c       Inicializa el buffer para el intercambio y envía la información
+c         Envia:
+c   	  - tid del proceso padre (al que hay que devolver al valor)
+c	      - tids de los procesos disponibles (esclavos)
+c         - número de tids
+c  	      - valor del input a incrementar
+	  	call pvmfinitsend(PVMDATADRAW, 1)
+	  	call pvmfpack(INTEGER4, mytid, 1, 1, info)
+		call pvmfpack(INTEGER4, nhost, 1, 1, info)
+		call pvmfpack(INTEGER4, tids, nhost, 1, info)
+		call pvmfpack(INTEGER4, var, 1, 1, info)
+		call pvmfsend(tids(i), 1, info)
+	  endif
 c Espera el resultado final del último exclavo
+	  
+	  call pvmfrecv(-1, 3, info)
+      call pvmfunpack(INTEGER4, var, 1, 1, info)
 
-	call pvmfmcast(nhost, tids, 1, info)
-
-
+	  print*, 'La suma del programa es: ', var
 
 c Sale de pvm y exit del programa
-	call pvmfexit( info)
-        print*, 'He salido de PVM; codigo de estado: ',info
-       	end
+	  call pvmfexit( info)
+      print*, 'He salido de PVM; codigo de estado: ',info
+      end
--- ANILLO/slave.f
+++ ANILLO/slave.f
@@ -1,4 +1,46 @@
-       	program slave
+c Programa slave - practica 1 programación paralela
+c Alumno Ivan Gomez
+      program slave
 	implicit none
-	print*, 'hola mundo'
-       	end
+	intenger mytid, rtid, tids, ntids, var, mpos, ntid
+        
+c Inicio el pvm y obtengo mi tid
+      call pvmfmytid( mytid )
+      print*, 'Esclavo: ', mytid
+
+c Obtengo la información
+c     - el tip del nodo master - rtid
+c     - número total de tids - ntids
+c     - el array de tids - tids
+c     - la variable a incrementar - var
+      call pvmfrecv(mytid, 1, info)
+      call pvmfunpack(INTEGER4, rtid, 1, 1, info)
+      call pvmfunpack(INTEGER4, ntids, 1, 1, info)
+      call pvmfunpack(INTEGER4, tids, ntids, 1, info)
+      call pvmfunpack(INTEGER4, var, 1, 1, info)
+
+c Incrementamos el valor de la variable de paso
+      var = var + 1
+
+      do i=1, ntids
+        if (mytid.eq.tids(i)) then
+          mpos=i
+        endif
+      enddo
+
+c Compruebo si soy el último y envío al master el resultado o envío al siguiente tid
+      if (mpos.eq.ntids) then
+c Soy el último
+        call pvmfinitsend(PVMDATADRAW, 1)
+        call pvmfpack(INTEGER4, var, 1, 1, info)
+        call pvmfsend(rtid, 1, info)
+      else
+c Envio al siguiente nodo del anillo
+        call pvmfinitsend(PVMDATADRAW, 1)
+        call pvmfpack(INTEGER4, mytid, 1, 1, info)
+        call pvmfpack(INTEGER4, tids, nhost, 1, info)
+        call pvmfpack(INTEGER4, nhost, 1, 1, info)
+        call pvmfpack(INTEGER4, var, 1, 1, info)
+        call pvmfsend(tids(mpos+1), 1, info)
+      endif
+      end
